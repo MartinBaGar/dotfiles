d5522def1b740f1c19579c4a30284fb01968cb00
MartinBaGar <martbari.g@gmail.com>
2025-04-01 15:46:22 +0200

    doom config files update

--- .config/.doom.d/config.el
+++ .config/.doom.d/config.el
@@ -11,17 +11,53 @@
 
 ;; remap <localleader> from SPC m to SPC l
 ;; (setq doom-localleader-key "SPC l"
-;;       doom-localleader-alt-key "M-SPC l")
+;;      doom-localleader-alt-key "M-SPC l")
+
+(setq doom-theme 'doom-gruvbox)
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 20))
+
+(setq display-line-numbers-type 'relative) ;; TODO change to 'visual in org-mode
+(add-hook! display-line-numbers-mode
+  (custom-set-faces!
+    '(line-number :slant normal)
+    '(line-number-current-line :slant normal)))
 
 (setq global-hl-line-modes nil)
 
 (setq display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 
+;; Run (package-vc-install '(ultra-scroll :vc-backend Git :url "https://github.com/jdtsmith/ultra-scroll")) in scratch buffer.
+;; (use-package! ultra-scroll
+;;   :init
+;;   (setq scroll-conservatively 101 ; important!
+;;         scroll-margin 0)
+;;   :config
+;;   (ultra-scroll-mode 1))
+
 (add-to-list 'default-frame-alist '(width . 100))
 (add-to-list 'default-frame-alist '(height . 40))
 
 (after! org
-(setq org-todo-keywords
-  '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE"))))
-(setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("laptop" . ?l)))
+  (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE"))))
+(setq org-tag-alist '(("labo" . ?l) ("maison" . ?m) ("production" . ?p) ("analyse" . ?a) ("biblio" . ?b) ("divers" . ?d)))
+
+;; Set default dictionary
+(setq ispell-dictionary "fr_FR")
+
+;; Create a function to select dictionary from a list
+(defun select-dictionary ()
+  "Select spelling dictionary."
+  (interactive)
+  (let* ((dicts '("en_US" "fr_FR"))
+         (selection (completing-read "Select dictionary: " dicts nil t)))
+    (ispell-change-dictionary selection)
+    (message "Dictionary switched to %s" selection)))
+
+;; Bind the selection function to a key
+(map! :leader
+      (:prefix ("t" . "toggle")
+       :desc "Select dictionary" "d" #'select-dictionary))
+
+(setq org-display-inline-images t)
+(setq org-display-remote-inline-images 'cache)
--- .config/.doom.d/custom.el
+++ .config/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/mnt/c/Users/martb/Documents/Obsidian/Org/1.org")))
+ '(org-agenda-files '("~/Documents/org/todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
