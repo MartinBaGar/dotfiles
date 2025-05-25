717dbaeda98f4cf5d4331ffde948e9102534dfe9
MartinBaGar <martbari.g@gmail.com>
2025-05-25 13:28:13 +0200

    updated config

--- .config/.doom.d/custom.el
+++ .config/.doom.d/custom.el
@@ -4,9 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7758a8b8912ef92e8950a4df461a4d510484b11df0d7195a8a3d003965127abc"
+     "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
  '(ede-project-directories
    '("/data/bari-garnier/endoU" "/home/bari-garnier/Documents/papiers"))
- '(org-agenda-files '("/home/bari-garnier/org/todo.org" "/home/mabagar/org/todo.org")))
+ '(org-agenda-files
+   '("/home/bari-garnier/org/todo.org" "/home/mabagar/org/todo.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
