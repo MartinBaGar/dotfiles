ddb4b73216ab7f62a00fc4a9976e71bf39774783
MartinBaGar <martbari.g@gmail.com>
2025-11-20 12:45:06 +0100

    changed hierarchy

--- doom/config.el
+++ doom/config.el
@@ -592,3 +592,20 @@ If FORCE-PROMPT is non-nil, always prompt for image file."
     (:name "Today's messages" :query "date:today..now AND NOT maildir:/gmail/[Gmail]/Spam" :key 116)
     (:name "Gmail No spam" :query "maildir:/gmail/INBOX AND NOT maildir:/gmail/[Gmail]/Spam" :key 119)
     (:name "Last 7 days" :query "date:7d..now AND NOT maildir:/gmail/[Gmail]/Spam" :hide-unread t :key 112))))
+
+;; credit: yorickvP on Github
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe
+                                      :noquery t))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
