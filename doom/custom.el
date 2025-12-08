08e35c10c1fff5df4afd6a10bce4331f0e3c6399
Martin Bari Garnier <martbari.g@gmail.com>
2025-12-08 18:15:15 +0100

    updated configs (mail/desktop/emacs)

--- doom/custom.el
+++ doom/custom.el
@@ -4,8 +4,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((tp--master-file . "/home/mabagar/docs/curriculum_vitae/curriculum_vitae.typ"))))
+ '(smtpmail-smtp-server "zmail.ibpc.fr")
+ '(smtpmail-smtp-service 587))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
